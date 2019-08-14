2a4505b149b966797751b6dd714ab3cfcb49927d
alokshakya <alokshakyacode@gmail.com>
2019-08-14 11:02:53 +0530

    preloading of all modules after eager loading of required modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 // import { LoginComponent } from './components/login/login.component';
 import { LoadoutComponent } from './components/account/loadout/loadout.component';
@@ -27,7 +27,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules}) 
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
